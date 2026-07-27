46a4b6c32c37794f570cfa6e458b25d86739099e
Sanchit Tanwar <tanwarsanchit9762@gmail.com>
2026-07-27 15:29:45 +0530

    revert: remove explicit IPv4 DNS resolution

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,6 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import dns from "node:dns/promises";
 import * as schema from "./schema";
 
 const DATABASE_URL = process.env.DATABASE_URL;
@@ -9,12 +8,8 @@ if (!DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is required");
 }
 
-const url = new URL(DATABASE_URL);
-const ipv4 = await dns.resolve4(url.hostname).then((r) => r[0]);
-url.hostname = ipv4;
-
 const pool = new pg.Pool({
-  connectionString: url.toString(),
+  connectionString: DATABASE_URL,
 });
 
 export const db = drizzle(pool, { schema });
